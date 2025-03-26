749ce2189ccbbf5b619ed69b252bec05b43189a1
SantiagoAMS <morales.soria.santiago.a@gmail.com>
2025-03-26 11:04:00 -0300

    Codigo casi finalizado

--- tp1_2.c
+++ tp1_2.c
@@ -1,19 +1,74 @@
 #include <stdio.h>
 
-int cuadrado(int n);
+int Cuadrado(int n);
+void CuadradoReferencia(int *n);
+void Invertir(int *a, int *b);
+void Orden(int *a, int *b);
+
 
 int main(){
     
+    int a, b;
+
+    printf("\nIngresa el valor \"a\": ");
+    scanf("%d",&a);
+    
+    printf("\nIngresa el valor \"b\": ");
+    scanf("%d",&b);
+    
+    printf("\n--------- CUADRADO POR VALOR ---------");
+    printf("\nCuadrado de a (%d): %d",a,cuadrado(a));
+    printf("\nCuadrado de a (%d): %d",b,cuadrado(b));
+    printf("\n--------- CUADRADO POR REFERENCIA ---------");
+    printf("\nCuadrado de a (%d): ",a);
+    cuadradoReferencia(&a);
+    printf("%d",a);
+    
+    printf("\nCuadrado de a (%d): ", b);
+    cuadradoReferencia(&b);
+    printf("%d",b);
+    
+    printf("\n--------- INVERSION ---------");
+    printf("\na = %d",a);
+    printf("\nb = %d",b);
+    printf("\n INVIERTO");
+    Invertir(&a,&b);
+    printf("\na = %d",a);
+    printf("\nb = %d",b);
+    
+    printf("\n--------- ORDENAR ---------");
+    printf("\na = %d",a);
+    printf("\nb = %d",b);
+    printf("\n REORDENO ");
+    Orden(&a,&b);
+    printf("\na = %d",a);
+    printf("\nb = %d",b);
     
     return 0;    
 }
 
-int cuadrado(int n){
+int Cuadrado(int n){
     return n*n;    
 }
 
-void cuadradoReferencia(int *n){
+void CuadradoReferencia(int *n){
+     printf("\nDireccion: %x",n);
+     printf("\nContenido: %d",*n);
      *n = (*n) * (*n);
+}
+
+void Invertir(int *a, int *b){
+     int c = *a;
+     *a = *b;
+     *b = c;
+}
+
+void Orden(int *a, int *b){
+     if (*a >*b){
+          int c = *a;          
+          *a = *b;
+          *b = c;
+     }
 }
 
 /*
